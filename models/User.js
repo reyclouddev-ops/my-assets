3e9e3facfe3a9ef542ba1df00aa6029bec766d16
reyclouddev-ops <reyclouddev@gmail.com>
2026-08-12 18:04:02 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,30 +1,37 @@
 const mongoose = require("mongoose")
 
-const UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-
-    password: {
-      type: String,
-      required: true
-    },
-
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user"
-    }
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    maxlength: 32
   },
-  {
-    timestamps: true
+  email: {
+    type: String,
+    default: ""
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: "User"
+  },
+  dnsUsed: {
+    type: Number,
+    default: 0
+  },
+  lastReset: {
+    type: Date,
+    default: null
   }
-)
+}, {
+  timestamps: true
+})
 
 module.exports =
   mongoose.models.User ||
